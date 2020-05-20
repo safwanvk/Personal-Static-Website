9c7a91db50a54bdbb1d6ba545fcb47c75366b5f0
safwan <safwanvalakundil@gmail.com>
2020-05-20 01:13:25 -0700

    first commit

--- sendemail/settings.py
+++ sendemail/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,13 +128,21 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, 'sendemail/static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 MEDIA_URL = '/images/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
 
