20867dff329204505d899a06e78953a58aaef511
safwanvk <safwanvalakundil@gmail.com>
2020-07-04 14:26:02 +0530

    Delete files in django

--- account/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import forms
-from django.db import models
-
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100),
-    email = models.EmailField(max_length=100),
-    subject = models.CharField(max_length=100),
-    message = models.TextField(max_length=250)
